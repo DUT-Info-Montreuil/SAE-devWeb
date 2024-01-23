2e5c62005ad44d877599399e4c6db841b2021ac4
YoannGit <lehongcheffsonyoann@gmail.com>
2024-01-23 15:02:56 +0100

    correctio footer

--- template.php
+++ template.php
@@ -2,7 +2,7 @@
 <html>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Titre de la Page</title>
+    <title>Page d'accueil</title>
     <link rel="stylesheet" type="text/css" href="css/style_index.css">
     <!--<link rel="stylesheet" href="css/styles.css">-->
 <head>
