456f49bab5e1faad95ce570bd7576d332919786a
ISAF Mehdi <m.isaf@iut.univ-paris8.fr>
2024-01-23 15:00:05 +0100

    modification du CSS

--- modules/mod_boutique/vue_boutique.php
+++ modules/mod_boutique/vue_boutique.php
@@ -4,18 +4,18 @@ require_once 'vue_generique.php';
 class VueBoutique extends VueGenerique { 
 
     public function afficherBoutique() {
-        echo'<link rel="stylesheet" href="css/styles_boutique.css">';
-      echo'<div class="content-container">
+        echo '<link rel="stylesheet" href="css/styles_boutique.css">';
+        echo '<div class="content-container">
         <div class="card-container">
-            <div class="card">
-                <img src="images/Boutique/bombe.png" alt="Objet  Bombe" class="card-image">
+            <div class="card-boutique">
+                <img src="images/Boutique/bombe.png" alt="Objet Bombe" class="card-image">
                 <div class="card-content">
                     <h2>Bombe</h2>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                 </div>
             </div>
 
-            <div class="card">
+            <div class="card-boutique">
                 <img src="images/Boutique/hydrogene.png" alt="Objet Hydrogène" class="card-image">
                 <div class="card-content">
                     <h2>Hydrogène</h2>
@@ -23,14 +23,13 @@ class VueBoutique extends VueGenerique {
                 </div>
             </div>
 
-            <div class="card">
+            <div class="card-boutique">
                 <img src="images/Boutique/mur.png" alt="Objet Mur" class="card-image">
                 <div class="card-content">
                     <h2>Mur</h2>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                 </div>
             </div>
-            
         </div>
     </div>';
        
