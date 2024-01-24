7eb87d9a4ef8880c62d1cd71baadf65de18a519b
rboulkriat <rababboulkriat@outlook.fr>
2024-01-24 22:03:08 +0100

    Modification de la première partie du design

--- modules/mod_debut/vue_debut.php
+++ modules/mod_debut/vue_debut.php
@@ -14,8 +14,14 @@ class VueDebut extends VueGenerique {
                 <p class="description">Découvrez un monde d\'aventures extraordinaires.</p>
 
                 <div class="grid-container">
-                <img src="images/profil/ennemis.png" alt="Description des aliens"  class="img-profil">
-                <p class="image-description">Explorez les confins de l\'univers avec notre plateforme de jeu immersive. Rencontrez des civilisations extraterrestres, participez à des quêtes épiques et découvrez des planètes inexplorées. Plongez dans l\'aventure dès maintenant et laissez-vous emporter par un monde de possibilités infinies. Rejoignez des milliers de joueurs passionnés et créez votre propre légende spatiale. Que l\'exploration commence !</p>
+                <img id="imageProfil" src="images/ennemis.png" alt="Description des aliens"  class="img-profil">
+                <div>
+                <p class="image-description">Préparez-vous à affronter des adversaires redoutables et découvrez les mystères des créatures intergalactiques qui peuplent notre univers !
+                Des hordes d\'ennemis extraterrestres n\'attendent que vous pour tester votre courage et vos compétences. Explorez des mondes hostiles, déjouez des stratégies machiavéliques, et devenez le héros de votre propre aventure spatiale.
+                Plongez dans l\'inconnu, affrontez des défis épiques, et forgez votre destin dans un univers où chaque rencontre est une opportunité de grandeur. Êtes-vous prêt à relever le défi ?</p>
+                <br> 
+                <a href="index.php?module=profil&action=profil"><button class="learn-more"><span class="circle" aria-hidden="true"><span class="icon arrow"></span></span><span class="button-text">Découvrir</span></button></a>
+                </div>
                 </div>
 
                 <div class="grid-container">
@@ -29,7 +35,7 @@ class VueDebut extends VueGenerique {
                 </div>
                 
                 <p class="description">Rejoignez la communauté des joueurs passionnés.</p>
-                <br> <!-- Ajoute un espace entre les lignes -->
+                <br> 
                 <a href="https://github.com/DUT-Info-Montreuil/SAE---devWeb.git" class = "style_bouton">Commencer l\'aventure</a>
 
                 <div class = "paragraphe">
@@ -38,6 +44,12 @@ class VueDebut extends VueGenerique {
                 </div>
 
             </div>';
+            
             echo '<script src="js/annimation.js"></script>';
+            echo '<script>
+            var imageProfil = document.getElementById(\'imageProfil\');
+            imageProfil.classList.add(\'show\');
+            </script>';
+            
     }
 }
