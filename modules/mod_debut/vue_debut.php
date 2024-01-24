8aea27cea786cda4e41f0940535ca83845cdc6a7
rboulkriat <rababboulkriat@outlook.fr>
2024-01-24 19:58:46 +0100

    modification du CSS

--- modules/mod_debut/vue_debut.php
+++ modules/mod_debut/vue_debut.php
@@ -5,6 +5,7 @@ require_once 'vue_generique.php';
 class VueDebut extends VueGenerique {
 
     public function afficherAccueil() {
+        echo '<link rel="stylesheet" href="css/style_accueil.css">';
         echo '<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Exo+2&display=swap">';
         echo '
             <div>
@@ -13,11 +14,22 @@ class VueDebut extends VueGenerique {
                 <p class="description">Découvrez un monde d\'aventures extraordinaires.</p>
 
                 <div class="grid-container">
-                <img src="images/aliensAccueilremoved.png" alt="Description des aliens" style="max-width: 100%; height: auto; margin: 20px;">
+                <img src="images/profil/ennemis.png" alt="Description des aliens"  class="img-profil">
                 <p class="image-description">Explorez les confins de l\'univers avec notre plateforme de jeu immersive. Rencontrez des civilisations extraterrestres, participez à des quêtes épiques et découvrez des planètes inexplorées. Plongez dans l\'aventure dès maintenant et laissez-vous emporter par un monde de possibilités infinies. Rejoignez des milliers de joueurs passionnés et créez votre propre légende spatiale. Que l\'exploration commence !</p>
                 </div>
 
+                <div class="grid-container">
+                <p class="image-description">Explorez les confins de l\'univers avec notre plateforme de jeu immersive. Rencontrez des civilisations extraterrestres, participez à des quêtes épiques et découvrez des planètes inexplorées. Plongez dans l\'aventure dès maintenant et laissez-vous emporter par un monde de possibilités infinies. Rejoignez des milliers de joueurs passionnés et créez votre propre légende spatiale. Que l\'exploration commence !</p>
+                <img src="images/profil/ennemis_tues.png" alt="Description des aliens"  class="img-profil">
+                </div>
+
+                <div class="grid-container">
+                <img src="images/profil/ennemis.png" alt="Description des aliens"  class="img-profil">
+                <p class="image-description">Explorez les confins de l\'univers avec notre plateforme de jeu immersive. Rencontrez des civilisations extraterrestres, participez à des quêtes épiques et découvrez des planètes inexplorées. Plongez dans l\'aventure dès maintenant et laissez-vous emporter par un monde de possibilités infinies. Rejoignez des milliers de joueurs passionnés et créez votre propre légende spatiale. Que l\'exploration commence !</p>
+                </div>
+                
                 <p class="description">Rejoignez la communauté des joueurs passionnés.</p>
+                <br> <!-- Ajoute un espace entre les lignes -->
                 <a href="https://github.com/DUT-Info-Montreuil/SAE---devWeb.git" class = "style_bouton">Commencer l\'aventure</a>
 
                 <div class = "paragraphe">
@@ -26,6 +38,6 @@ class VueDebut extends VueGenerique {
                 </div>
 
             </div>';
-            echo '<script src="js/annimation.js"></script>'; // titre h1
+            echo '<script src="js/annimation.js"></script>';
     }
 }
