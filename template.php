ab9a4ae51e600e378c5b673e98d8ea1599bf59b1
rboulkriat <rababboulkriat@outlook.fr>
2024-01-25 19:45:24 +0100

    correction des derniers fichiers

--- template.php
+++ template.php
@@ -6,28 +6,6 @@
     <title>Titre de la Page</title>
     <link rel="stylesheet" type="text/css" href="css/style_accueil.css">
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-
-    <script>
-        // Fonction pour afficher le message dans une boîte de dialogue
-        function afficherMessage(message) {
-            alert(message);
-        }
-
-        // Code pour soumettre le formulaire via AJAX
-        document.addEventListener('DOMContentLoaded', function() {
-            document.querySelector('form').addEventListener('submit', function(e) {
-                e.preventDefault();
-
-                // Utilisez ici la méthode AJAX pour soumettre le formulaire et récupérer la réponse
-
-                // Supposons que "reponse" contienne la réponse JSON de votre serveur
-                var reponse = { "message": "" };
-
-                // Affichez le message dans une boîte de dialogue
-                afficherMessage(reponse.message);
-            });
-        });
-    </script>
 <head>
     
 </head>
